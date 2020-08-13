75660c7051e069e657a930926b9d250d10872cd5
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2020-08-13 16:04:52 -0600

    HOLA MUNDO
    SE AGREGÓ UN HOLA MUNDO

--- Clase 1/src/clase/pkg1/Clase1.java
+++ Clase 1/src/clase/pkg1/Clase1.java
@@ -15,7 +15,7 @@ public class Clase1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola GIT-HUB");
     }
     
 }
